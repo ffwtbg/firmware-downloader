ae9397b33b08e6137059e4c2c326c2cd62e1567e
robin <robin@ut-team.de>
2016-05-10 20:45:28 +0200

    wtbg

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -10,14 +10,31 @@ $(document).ready(function() {
 		var	type = '',
 			fileExtension = '',
 			fileEnding = '.bin',
-			branchdir = 'stable',
-			vnumber = '0.6.2-20150417',
+			vnumber = '0.8.5-L2TP',
 			router,
 			region;
 		router = $('#download-form-router').val();
 		region = $('#download-form-region').val();
+		
+		if (region == 'winterberg_doerfer')
+			{
+				region_short = 'doerfer';
+			}
+		if (region == 'winterberg_kernstadt')
+			{
+				region_short = 'kernstadt';
+			}
+		if (region == 'medebach')
+			{
+				region_short = 'mdb';
+			}
+		if (region == 'hallenberg')
+			{
+				region_short = 'hlb';
+			}
 
-		var siteCode = 'ffgl-' + region;
+		var siteCode = 'ffwtbg_' + region_short;
+		
 
 		switch ($('#download-form-type').val()) {
 			case '0':
@@ -36,30 +53,13 @@ $(document).ready(function() {
 				type = 'factory';
 		}
 
-			/*switch ($('#branch').val()) {
-				case '0':
-					branchdir = 'stable';
-					vnumber = '0.6.2-';
-					break;
-				case '1':
-					branchdir = 'beta';
-					vnumber = '0.6~beta-2';
-					break;
-				case '2':
-					branchdir = 'experimental';
-					vnumber = '0.6.1~exp20150122';
-					break;
-				default:
-					branchdir = 'stable';
-					vnumber = '0.6';
-			}*/
-
+		
 		if(region === '-1') {
 			window.alert('Bitte wähle eine Region aus.');
 		} else if(router === '-1') {
 			window.alert('Bitte wähle einen Router aus.');
 		} else {
-			window.location.href = region+'/'+branchdir+'/'+type+'/gluon-'+siteCode+'-'+vnumber+'-'+router+fileExtension+fileEnding;
+			window.location.href = 'http://images.freifunk-winterberg.net/'+region+'/'+type+'/gluon-'+siteCode+'-'+vnumber+'-'+router+fileExtension+fileEnding
 		}
 
 		return false;
